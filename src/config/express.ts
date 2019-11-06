3e1639f2080daa633eb37c4d762418e0946e151c
ajantha <ajantha@wired2perform.com>
2019-11-06 09:42:43 +0530

    added airgram

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,8 +1,42 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Airgram, Auth, prompt, toObject } from 'airgram';
 
 // routes
 import userRoute from '../routes/user.route';
+
+const initAirGram = () => {
+  const airgram = new Airgram({
+    apiId: 1184698,
+    apiHash: 'd6503f3e6ffb2a84d59d519cef4d16cb',
+    command: process.env.TDLIB_COMMAND,
+    logVerbosityLevel: 2
+  });
+
+  airgram.use(new Auth({
+    code: () => prompt(`Please enter the secret code:\n`),
+    phoneNumber: () => prompt(`Please enter your phone number:\n`)
+  }))
+  
+  void (async () => {
+    const me = toObject(await airgram.api.getMe())
+    console.log(`[Me] `, me)
+  })
+  
+  // Getting all updates
+  airgram.use((ctx: any, next: any) => {
+    if ('update' in ctx) {
+      console.log(`[all updates][${ctx._}]`, JSON.stringify(ctx.update))
+    }
+    return next()
+  })
+  
+  // Getting new messages
+  airgram.on('updateNewMessage', async ({ update } : any) => {
+    const { message } = update
+    console.log('[new message]', message)
+  })
+}
 
 const initAppConfigs = (app: any) => {
   // setup the body-parser configurations
@@ -36,6 +70,8 @@ const init = (): any => {
   initAppConfigs(app);
   initServerRoutes(app);
   initErrorHandler(app);
+
+  initAirGram();
 
   return app;
 }
